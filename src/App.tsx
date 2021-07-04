77046aeb2eef8c65aedb53c37990d7a8539cb7f5
Manuel Cocoba <manuelcocoba@gmail.com>
2021-07-04 15:39:22 -0700

    add credits section

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React, { lazy, Suspense } from 'react';
 import WeatherSkeletonCard from './components/WeatherCard/WeatherSkeletonCard';
 
 const WeatherGrid = lazy(() => import('./components/WeatherGrid'));
+const Credits = lazy(() => import('./components/Credits'));
 
 function App(): React.ReactElement {
   return (
     <Suspense fallback={<WeatherSkeletonCard />}>
       <WeatherGrid />
+      <Credits />
     </Suspense>
   );
 }
---
+++ src/components/Credits/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './Styles.scss';
+
+const Credits: React.FC = () => (
+  <>
+    <div className="footer">
+      <span>
+        Made with ❤️ by
+        <a href="https://manuelcocoba.com" target="_blank" rel="noreferrer">
+          Manuel Cocoba
+        </a>
+      </span>
+    </div>
+  </>
+);
+
+export default Credits;
